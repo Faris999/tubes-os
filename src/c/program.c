c0f916b3a2d544f4222d36b1da209e265f8009d2
Faris Hafizhan Hakim <farishafiz999@gmail.com>
2022-04-30 16:31:10 +0700

    Revert "parsing multiple program"
    This reverts commit af5ebbb87defcd35cd91769fb67770d5a116c2a5.

--- src/c/program.c
+++ src/c/program.c
@@ -11,79 +11,11 @@ void exec(struct file_metadata *metadata, int segment) {
 
 void exit() {
     struct file_metadata metadata;
-    struct message msg;
-    char *command;
-    char program[64];
-    char *rest;
-    int i;
+    // struct message msg;
 
-    get_message(&msg);
-
-    if (msg.other[0] == 0) {
-        metadata.node_name = "shell";
-        metadata.parent_index = 0x00;
-        exec(&metadata, 0x2000);
-        return;
-    }
-    
-    // command = msg.other;
-    // for (i = 0; i < 64; i++) {
-    //   if (msg.other[i] == '\0') {
-    //     rest = msg.other + i;
-    //     break;
-    //   }
-    //   if (startswith(" ; ", msg.other + i)) {
-    //     msg.other[i] = '\0';
-    //     rest = msg.other + i + 3;
-    //     break;
-    //   }
-    // }
-    // puts("command: ");
-    // puts(command);
-    // puts("\r\n");
-
-    // clear(msg.arg1);
-    // clear(msg.arg2);
-    // clear(msg.arg3);
-    
-    // i = 0;
-    // while (command[0] != '\0' && command[0] != ' ') {
-    //   program[i] = command[0];
-    //   command++;
-    //   i++;
-    // }
-    // program[i] = '\0';
-
-    // if (command[0] == ' ') {
-    //   command++;
-    //   i = 0;
-    //   while (command[0] != '\0' && command[0] != ' ') {
-    //     msg.arg1[i] = command[0];
-    //     command++;
-    //     i++;
-    //   }
-    //   msg.arg1[i] = '\0';
-
-    //   if (command[0] == ' ') {
-    //     command++;
-    //     i = 0;
-    //     while (command[0] != '\0') {
-    //       msg.arg2[i] = command[0];
-    //       command++;
-    //       i++;
-    //     }
-    //     msg.arg2[i] = '\0';
-    //   }
-    // }
-    msg.next_program_segment = msg.next_program_segment + 0x1000;
-
-    write_message(&msg);
     metadata.node_name = "shell";
-    // puts("executing: ");
-    // puts(program);
-    // puts("\r\n");
     metadata.parent_index = 0x00;
-    exec(&metadata, msg.next_program_segment - 0x1000);
+    exec(&metadata, 0x2000);
 }
 
 void write_message(struct message *msg) {
