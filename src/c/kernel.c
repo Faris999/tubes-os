62313a2bc4fac04be5b7d90708ae38b1579e7c84
Faris Hafizhan Hakim <farishafiz999@gmail.com>
2022-04-30 17:08:41 +0700

    milestone 1

--- src/c/kernel.c
+++ src/c/kernel.c
@@ -1,7 +1,73 @@
-// Kode kernel
-// PENTING : FUNGSI PERTAMA YANG DIDEFINISIKAN ADALAH main(),
-//   cek spesifikasi untuk informasi lebih lanjut
-
-// TODO : Tambahkan implementasi kode C
-
 #include "header/kernel.h"
+
+void printChar(char c);
+
+int main() {
+  char buf[128];
+  clearScreen();
+  makeInterrupt21();
+  printString("Halo dunia!\r\n");
+  readString(buf);
+  printString(buf);
+  while (true);
+}
+
+void handleInterrupt21(int AX, int BX, int CX, int DX) {
+  switch (AX) {
+    case 0x0:
+      printString(BX);
+      break;
+    case 0x1:
+      readString(BX);
+      break;
+    default:
+      printString("Invalid interrupt");
+  }
+}
+
+void printChar(char c) {
+  interrupt(0x10, 0x0e00 + c, 0, 0, 0);
+}
+
+void printString(char *string) {
+  int i;
+  for (i = 0; i < strlen(string); i++) {
+    printChar(string[i]);
+  }
+}
+
+void readString(char *string) {
+  int i = 0;
+  while (i < 128) {
+    byte AL = interrupt(0x16, 0x0, 0x0, 0x0, 0x0);
+    if (AL >= 0x20 && AL <= 0x7e) {
+      string[i] = AL;
+      printChar(AL);
+      i++;
+      continue;
+    }
+    if (AL == 0x8 && i > 0) {
+      string[--i] = 0x0;
+      printChar(0x8); 
+      interrupt(0x10, 0x0a20, 0x0, 0x1, 0x0);
+    }
+    if (AL == 0xd) {
+      printChar(0xd);
+      printChar(0xa);
+      string[i] = '\0';
+      break;
+    }
+  }
+}
+
+void clearScreen() {
+  int i;
+  int j;
+  for (i = 0; i < 80; i++) {
+    for (j = 0; j < 25; j++) {
+      putInMemory(0xB000, 0x8000 + (80 * j + i) * 2, 0x20);
+      putInMemory(0xB000, 0x8001 + (80 * j + i) * 2, 0xF);
+    }
+  }
+  interrupt(0x10, 0x0200, 0x03, 0x0, 0x0000); 
+}
--- src/c/std_lib.c
+++ src/c/std_lib.c
@@ -1,3 +1,58 @@
 #include "header/std_lib.h"
 
 // Opsional : Tambahkan implementasi kode C
+int div(int a, int b) {
+    return a / b;
+}
+
+int mod(int a, int n) {
+    while (a >= n) {
+        a -= n;
+    }
+    return a;
+}
+
+
+// Operasi standar bahasa C
+// Implementasikan
+void memcpy(byte *dest, byte *src, unsigned int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+        dest[i] = src[i];
+    }
+}
+// Mengcopy n bytes yang ditunjuk src ke dest
+
+unsigned int strlen(char *string) {
+    int i;
+    for (i = 0; string[i] != '\0'; i++);
+    return i;
+}
+// Mengembalikan panjang suatu null terminated string
+
+bool strcmp(char *s1, char *s2) {
+    int i;
+    for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++) {
+        if (s1[i] != s2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+// Mengembalikan true jika string sama
+
+void strcpy(char *dst, char *src) {
+    int i;
+    for (i = 0; src[i] != '\0'; i++) {
+        dst[i] = src[i];
+    }
+    dst[i] = '\0';
+}
+// Melakukan penyalinan null terminated string
+
+void clear(byte *ptr, unsigned int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+        ptr[i] = 0;
+    }
+}
